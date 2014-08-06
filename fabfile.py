ebeb1ec391302297de99d595c882816cd8705b69
Zoom.Quiet <zoomquiet+git@gmail.com>
2014-08-06 21:14:55 +0800

    upgraded in local. @MBP111216ZQ

--- fabfile.py
+++ fabfile.py
@@ -8,21 +8,28 @@ from fabric.api import (local
 env.hosts = ['23.239.0.15']
 env.port = 22
 env.user = 'zoomq'
-code_dir = '~/gae'
+code_dir = '~/gae/urisago1'
+gae_dir = '~/gae'
 
 env.deploy_path = './'
 
 def deploy():
-    git4pu()
-    gae2sora()
+    loc0pu()
+    git2pl
+    gae4up()
 
-def gae2sora():
+
+def git2pl():
     with cd(code_dir):
         run('pwd')
         run('git pull')
+
+def gae4up():
+    with cd(gae_dir):
+        run('pwd')
         run('/opt/sbin/gae4go/appcfg.py update urisago1/')
 
-def git4pu():
+def loc0pu():
     local('cd {deploy_path} && '
             'git add --all . && '
             'git ci -am "upgraded in local. @MBP111216ZQ" && '
